a59c36d5b3c798c3f1411d0ce7daf2bec8466ecf
jarvizshen <2189624196@qq.com>
2022-02-28 15:20:29 +0800

    day1

--- app/src/main/java/com/example/androidtest/MainActivity.java
+++ app/src/main/java/com/example/androidtest/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.androidtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button trueBtn = findViewById(R.id.true_button);
+        final Button falseBtn = findViewById(R.id.false_button);
+        trueBtn.setOnClickListener(view -> {
+            Toast.makeText(this,R.string.true_message,Toast.LENGTH_SHORT).show();
+        });
+        falseBtn.setOnClickListener(view -> {
+            Toast.makeText(this,R.string.false_message,Toast.LENGTH_SHORT).show();
+        });
     }
 }
